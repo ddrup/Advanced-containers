1c250271c8a17d1fd218eba467f201d011c9154d
vadoshor <vadoshor@student.21-school.ru>
2024-11-26 11:25:51 +0300

    stack_allocator impl

---
+++ common_containers/seq_container/list/allocator.h
@@ -0,0 +1,84 @@
+#ifndef CPP2_S21_CONTAINERS_SRC_COMMON_CONTAINERS_SEQ_CONTAINER_ALLOCATOR_ALLOCATOR_H_
+#define CPP2_S21_CONTAINERS_SRC_COMMON_CONTAINERS_SEQ_CONTAINER_ALLOCATOR_ALLOCATOR_H_
+
+#include <memory>
+
+namespace s21 {
+
+template <size_t N> 
+class StackStorage {
+private:
+  char buf_[N];
+  size_t reserved_ = 0;
+
+public:
+  StackStorage() = default;
+  StackStorage(const StackStorage &) = delete;
+  StackStorage(StackStorage &&) = delete;
+  void reserve(size_t n) {
+    reserved_ += n;
+  }
+  size_t get_reserved() const {
+    return reserved_;
+  }
+  char* get_buf() {
+    return buf_;
+  }
+};
+
+template <typename T, size_t N> 
+class StackAllocator {
+public:
+  using value_type = T;
+  using pointer = T*;
+  using size_type = std::size_t;
+  
+  template <size_t U>
+  explicit StackAllocator(StackStorage<U>& storage)
+    : storage_(&storage) {}
+
+  template <typename U>
+  StackAllocator(const StackAllocator<U,N>& other) noexcept
+    : storage_(other.storage_) {}
+
+  template <typename U>
+  StackAllocator& operator=(const StackAllocator<U,N>& other) noexcept {
+    if(this != &other) {
+      storage_ = other.storage_;
+    }
+    return *this;
+  } 
+
+  [[nodiscard]] pointer allocate(size_type n) {
+    size_type available_space = N - storage_->get_reserved();
+    void* ptr = static_cast<void*>(storage_->get_buf() + storage_->get_reserved());
+    void* aligned_ptr = std::align(alignof(T), n * sizeof(T), ptr, available_space);
+    
+    if(aligned_ptr == nullptr) {
+        throw std::bad_alloc();
+    }
+
+    storage_->reserve(static_cast<char*>(aligned_ptr) - storage_->get_buf() + n * sizeof(T));
+    return static_cast<T*>(aligned_ptr);
+  }
+
+  void deallocate(pointer ptr, size_type n){
+    std::ignore = ptr;
+    std::ignore = n;
+  }
+
+  template <typename U>
+	StackAllocator(StackAllocator<U, N>) {}
+	
+	template <typename U>
+	struct rebind {
+		using other = StackAllocator<U, N>; 
+	};
+
+private:
+  StackStorage<N>* storage_;
+};
+
+} // namespace s21
+
+#endif // CPP2_S21_CONTAINERS_SRC_COMMON_CONTAINERS_SEQ_CONTAINER_ALLOCATOR_ALLOCATOR_H_
---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+#include "common_containers/seq_container/list/allocator.h"
+
+#include <chrono>
+#include <iostream>
+#include <string>
+
+class LogDuration {
+public:
+    explicit LogDuration(const std::string& msg = "")
+        : message_(msg), start_(std::chrono::steady_clock::now()) {}
+
+    ~LogDuration() {
+        using namespace std::chrono;
+        auto finish = steady_clock::now();
+        auto duration = duration_cast<milliseconds>(finish - start_).count();
+        std::cerr << message_ << " took " << duration << " ms\n";
+    }
+
+private:
+    std::string message_;
+    std::chrono::steady_clock::time_point start_;
+};
+
+#define LOG_DURATION(message) LogDuration guard(message);
+
+void test_stack_allocator() {
+    s21::StackStorage<100000> storage;
+    s21::StackAllocator<int, 100000> stack_alloc(storage);
+
+    LOG_DURATION("StackAllocator test");
+    std::vector<int, s21::StackAllocator<int, 100000>> v(stack_alloc);
+
+    for (int i = 0; i < 10000; ++i) {
+        v.push_back(i);
+    }
+}
+
+void test_std_allocator() {
+    LOG_DURATION("std::allocator test");
+    std::vector<int> v;
+
+    for (int i = 0; i < 10000; ++i) {
+        v.push_back(i);
+    }
+}
+
+int main() {
+    test_stack_allocator();
+    test_std_allocator();
+    return 0;
+}
